a31116b806f9b013a602226f9efe073193e6806f
hatdaunho <lebadung178@gmail.com>
2025-09-10 22:19:35 +0700

    sắp xếp theo tiêu chí

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -7,7 +7,16 @@ module.exports.index = async (req, res) => {
   if(req.query.status) {
     find.status = req.query.status;
   }
-  
+
+  // Sort
+  const sort = {};
+  if(req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
+
   res.json(tasks);
 }
 
