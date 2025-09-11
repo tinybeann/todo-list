8a0f673f397983f730d80ea74659ff72a6e44f1e
hatdaunho <lebadung178@gmail.com>
2025-09-11 23:06:06 +0700

    cài đặt cors

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require("express");
 require("dotenv").config();
 const database = require("./config/database");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 database.connect();
 
 const app = express();
 const port = 3000;
+
+// // Tất cả tên miền được phép truy cập vào
+app.use(cors());
+
+// Cho phép 1 tên miền cụ thể được phép truy cập
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200
+// }
+// cors(corsOptions);
 
 // parse application/json
 app.use(bodyParser.json());
