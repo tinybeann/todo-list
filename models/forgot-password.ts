dcc6d8558cb8a0b2c140088e2a8ac541a82a4a23
Nguyễn Thế Anh <theanh11052006@gmail.com>
2025-09-13 23:20:14 +0700

    feat: schema types

---
+++ models/forgot-password.ts
@@ -0,0 +1,6 @@
+export type ForgotPassword = {
+  email: string;
+  otp: string;
+  expireAt: string;
+  timestamps: string;
+};
---
+++ models/user.ts
@@ -0,0 +1,9 @@
+export type User = {
+  fullName: string;
+  email: string;
+  password: string;
+  token: string;
+  deleted: boolean;
+  deletedAt: string;
+  timestamps: string;
+};
